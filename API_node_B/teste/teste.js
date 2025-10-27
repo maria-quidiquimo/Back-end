1ab4b9a87fb9e5f5b51eaf129f633f9e3970d518
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2025-10-27 15:28:14 -0300

    alteração no código, instalação do node e só

---
+++ API_node_B/teste/teste.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+const products = []
+app.use(express.json())
+app.get('/produtos', (req, res) => {
+    res.json(products)
+})
+app.post('/produtos', (res, req) => {
+    products.push(req.body)
+    res.json(req.body)
+})
+app.listen(8000, () => console.log('Servidor Funcionando'))
