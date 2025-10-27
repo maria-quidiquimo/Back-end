10b0ac9176227087eeb8f2049479aa7157d78744
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2025-10-27 14:00:26 -0300

    completando o código

--- API_node_B/server.js
+++ API_node_B/server.js
@@ -8,4 +8,10 @@ app.get('/usuarios', (req, res) => {
     res.json(users)
 })
 
+app.post('/usuarios', (req,res) => {
+    users.push(req.body)
+
+    res.json(req.body)
+})
+
 app.listen(3000, () => console.log('Servidor Rodando!!'))
