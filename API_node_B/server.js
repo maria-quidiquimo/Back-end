b2887788fad9d6da34b4e7e0c75c86b4914af25f
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2025-10-27 13:41:07 -0300

    mais linha de código sendo colocada

--- API_node_B/server.js
+++ API_node_B/server.js
@@ -1,5 +1,11 @@
 import express from 'express'
 
 const app = express()
+ 
+const users = []
+
+app.get('/usuarios', (req, res) => {
+    
+})
 
 app.listen(3000, () => console.log('Servidor Rodando!!'))
